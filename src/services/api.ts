a85e12022d677cf36b4e27a4ca771d8c14d3d3f5
Soufiane Majdar <soufiane.majdar@gmail.com>
2024-12-24 16:00:24 +0100

    new update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { FormData } from '../types';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://crop-optimizer-backend.onrender.com/api/';
+const API_BASE_URL ='https://crop-optimizer-backend.onrender.com/api/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
